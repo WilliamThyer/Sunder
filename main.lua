0c2bcc6bead38031f8a7637267f32378ee4c87e5
WilliamThyer <thyerwilliam@gmail.com>
2025-01-13 18:25:51 -0600

    fix

--- main.lua
+++ main.lua
@@ -2,12 +2,11 @@ if arg[#arg] == "vsc_debug" then require("lldebugger").start() end
 io.stdout:setvbuf("no")
 
 _G.love = require("love")
-anim8 = require 'libraries/anim8'
+_G.anim8 = require 'libraries/anim8'
 local Player = require("Player")
 local PlayerHelper = require("PlayerHelper")
 
 -- print(("Player 1 is hurt:%s"):format(player1.isHurt))
-print('starting game')
 
 function love.load()
   love.graphics.setDefaultFilter("nearest", "nearest")
